d38e63917884734e6a26604b820002c640c8e692
Teddy Wing <git@teddywing.com>
2020-11-11 21:39:56 +0100

    icns-millefeuille.sh: Use `sips` long option name
    For better readability.

--- icns-millefeuille.sh
+++ icns-millefeuille.sh
@@ -39,11 +39,11 @@ cd "$iconset"
 
 cp icon_16x16\@2x.png icon_32x32.png
 
-sips -Z 512 icon_512x512\@2x.png --out icon_512x512.png
-sips -Z 256 icon_256x256\@2x.png --out icon_256x256.png
-sips -Z 128 icon_128x128\@2x.png --out icon_128x128.png
-sips -Z 64 icon_128x128.png --out icon_32x32\@2x.png
-sips -Z 16 icon_16x16\@2x.png --out icon_16x16.png
+sips --resampleHeightWidthMax 512 icon_512x512\@2x.png --out icon_512x512.png
+sips --resampleHeightWidthMax 256 icon_256x256\@2x.png --out icon_256x256.png
+sips --resampleHeightWidthMax 128 icon_128x128\@2x.png --out icon_128x128.png
+sips --resampleHeightWidthMax 64 icon_128x128.png --out icon_32x32\@2x.png
+sips --resampleHeightWidthMax 16 icon_16x16\@2x.png --out icon_16x16.png
 
 cd ..
 
